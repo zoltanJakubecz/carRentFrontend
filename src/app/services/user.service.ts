a4ca7363aa3c7ba2956b8fd583f60b3585e540f9
Zoltan Jakubecz <jakuzo@gmail.com>
2020-12-15 21:25:05 +0100

    Add login method

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from '../models/User';
+import {UserAuth} from '../models/UserAuth';
 
 @Injectable({
   providedIn: 'root'
@@ -21,5 +22,9 @@ export class UserService {
 
   public register(user: User) {
     return this.http.post<User>(this.usersUrl, user);
+  }
+
+  public login(user: UserAuth){
+    console.log(user);
   }
 }
